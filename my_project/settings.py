9430603f0a1e162358e98f326e758cef247a652a
OSawani <DELETED-1699439824325>
2024-05-10 11:20:33 +0000

    experiment to see what information about resource locations, file paths, configuration options and db queries are revealed by DEBUG True

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-vypt2nsq(^wiq2rzx^(2-ed$4i=^4-520ng!k+z6s60wv)6(=@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-osawani-django-project-dici3wkzpo.us1.codeanyapp.com', '.herokuapp.com']
 
